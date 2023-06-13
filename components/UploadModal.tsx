8baebf1d50f217c3cb63747a056aceccf6bfc9de
Colin Hunwald <colin.hunwald@gmail.com>
2023-06-13 15:24:45 +0200

    Input continue

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -1,17 +1,25 @@
-'use client';
+"use client";
 
-import useUploadModal from '@/hooks/useUploadModal';
-import { useForm, SubmitHandler, FieldValues } from 'react-hook-form';
-import Modal from './Modal';
-import { useState } from 'react';
+import useUploadModal from "@/hooks/useUploadModal";
+import { useForm, SubmitHandler, FieldValues } from "react-hook-form";
+import Modal from "./Modal";
+import { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
+import { toast } from "react-hot-toast";
+import { useUser } from "@/hooks/useUser";
+import uniqid from "uniqid";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 
 const UploadModal = () => {
-  const [isLoading, setIsLoading] = useState()
+  const [isLoading, setIsLoading] = useState(false);
   const uploadModal = useUploadModal();
+  const { user } = useUser();
+  const supabaseClient = useSupabaseClient();
   const { register, handleSubmit, reset } = useForm<FieldValues>({
     defaultValues: {
-      author: '',
-      title: '',
+      author: "",
+      title: "",
       song: null,
       image: null,
     },
@@ -22,9 +30,22 @@ const UploadModal = () => {
       uploadModal.onClose();
     }
   };
-  const onSubmit: SubmitHandler<FieldValues> = (values) => async {
-    // Upload to supabase
-  }
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {
+    try {
+      setIsLoading(true);
+      const imageFile = values.image?.[0];
+      const songFile = values.song?.[0];
+      if (!imageFile || !songFile || !user) {
+        toast.error("Missing fields");
+        return;
+      }
+      const uniqueID = uniqid();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <Modal
       title="Add a song"
@@ -32,8 +53,42 @@ const UploadModal = () => {
       isOpen={uploadModal.isOpen}
       onChange={onChange}
     >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Input id="title" disabled={isLoading} {...register('title', {required: true})} placeholder="Song title" />
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-y-4">
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+        <Input
+          id="author"
+          disabled={isLoading}
+          {...register("author", { required: true })}
+          placeholder="Song author"
+        />
+        <div>
+          <div className="pb-1">Select a song file!</div>
+          <Input
+            id="song"
+            type="file"
+            disabled={isLoading}
+            accept=".mp3"
+            {...register("song", { required: true })}
+          />
+        </div>
+        <div>
+          <div className="pb-1">Select an image!</div>
+          <Input
+            id="image"
+            type="file"
+            disabled={isLoading}
+            accept="image/*"
+            {...register("image", { required: true })}
+          />
+        </div>
+        <Button disabled={isLoading} type="submit">
+          Create
+        </Button>
       </form>
     </Modal>
   );
