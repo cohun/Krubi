a459a7cfe30f8c72b46b796580f5b67f4c74df52
Colin Hunwald <colin.hunwald@gmail.com>
2023-06-15 15:24:12 +0200

    Add loadImages

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,6 +1,7 @@
 import getSongs from "@/actions/getSongs";
 import Header from "@/components/Header";
 import ListItem from "@/components/ListItem";
+import PageContent from "./components/PageContent";
 
 export const revalidate = 0;
 export default async function Home() {
@@ -23,11 +24,7 @@ export default async function Home() {
         <div className="flex justify-between items-center">
           <h1 className="text-white font-semibold text-2xl">Newest songs</h1>
         </div>
-        <div>
-          {songs.map((song) => (
-            <div key={song.id}>{song.title}</div>
-          ))}
-        </div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
