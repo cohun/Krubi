fdb68bc2969e2ad08fabc763be61130ebe44fd33
Colin Hunwald <colin.hunwald@gmail.com>
2023-06-16 12:18:36 +0200

    getSongsByUserId

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,31 @@
-import Sidebar from '@/components/Sidebar';
-import './globals.css';
-import { Figtree } from 'next/font/google';
-import SupabaseProvider from '@/providers/SupabaseProvider';
-import UserProvider from '@/providers/UserProvider';
-import ModalProvider from '@/providers/ModalProvider';
-import ToasterProvider from '@/providers/ToasterProvider';
+import Sidebar from "@/components/Sidebar";
+import "./globals.css";
+import { Figtree } from "next/font/google";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
+import getSongsByUserId from "@/actions/getSongsByUserId";
 
-const font = Figtree({ subsets: ['latin'] });
+const font = Figtree({ subsets: ["latin"] });
 
 export const metadata = {
   languages: {
-    'en-US': '/en-US',
-    'hu-HU': '/hu-HU',
+    "en-US": "/en-US",
+    "hu-HU": "/hu-HU",
   },
-  title: 'Krúbi Website',
-  description: 'Magyarország legjobb rappere',
-  keywords: ['rapper', 'zene', 'koncert'],
+  title: "Krúbi Website",
+  description: "Magyarország legjobb rappere",
+  keywords: ["rapper", "zene", "koncert"],
 };
+export const revalidate = 0;
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const userSongs = await getSongsByUserId();
   return (
     <html lang="hu">
       <body className={font.className}>
@@ -30,7 +33,7 @@ export default function RootLayout({
         <SupabaseProvider>
           <UserProvider>
             <ModalProvider />
-            <Sidebar>{children}</Sidebar>
+            <Sidebar songs={userSongs}>{children}</Sidebar>
           </UserProvider>
         </SupabaseProvider>
       </body>
--- components/Library.tsx
+++ components/Library.tsx
@@ -1,12 +1,16 @@
-'use client';
+"use client";
 
-import { TbPlaylist } from 'react-icons/tb';
-import { AiOutlinePlus } from 'react-icons/ai';
-import useAuthModal from '@/hooks/useAuthModal';
-import { useUser } from '@/hooks/useUser';
-import useUploadModal from '@/hooks/useUploadModal';
+import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useUser } from "@/hooks/useUser";
+import useUploadModal from "@/hooks/useUploadModal";
+import { Song } from "@/types";
 
-const Library = () => {
+interface LibraryProps {
+  songs: Song[];
+}
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const authModal = useAuthModal();
   const uploadModal = useUploadModal();
   const { user } = useUser();
@@ -17,6 +21,7 @@ const Library = () => {
     // TODO: Check for subscription
     return uploadModal.onOpen();
   };
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
@@ -30,7 +35,11 @@ const Library = () => {
           className="transition cursor-pointer text-neutral-400 hover:text-white"
         />
       </div>
-      <div className="flex flex-col px-3 mt-4 gap-y-2">List of Songs!</div>
+      <div className="flex flex-col px-3 mt-4 gap-y-2">
+        {songs.map((song) => (
+          <div key={song.id}>{song.title}</div>
+        ))}
+      </div>
     </div>
   );
 };
