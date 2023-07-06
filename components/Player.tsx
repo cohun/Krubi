ee67aa5290e6193534549dccb41c678c7a26fa8b
Colin Hunwald <colin.hunwald@gmail.com>
2023-07-06 15:17:05 +0200

    PlayerContent

--- components/Player.tsx
+++ components/Player.tsx
@@ -3,6 +3,7 @@
 import useGetSongById from "@/hooks/useGetSongById";
 import usePlayer from "@/hooks/UsePlayer";
 import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import PlayerContent from "./PlayerContent";
 
 const Player = () => {
   const player = usePlayer();
@@ -26,7 +27,7 @@ const Player = () => {
         px-4
       "
     >
-      Player!
+      <PlayerContent key={songUrl} song={song} songUrl={songUrl} />
     </div>
   );
 };
